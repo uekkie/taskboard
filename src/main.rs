38ab171504c529883c190a9234fcfd42a17cada8
Hiroshi Ueki <ueki@sonicgarden.jp>
2025-04-04 10:06:51 +0900

    カウントアップできるまで

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 use leptos::prelude::*;
+use leptos::mount::mount_to_body;
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    console_error_panic_hook::set_once();
+    mount_to_body(Counter)
+}
+
+
+#[component]
+fn Counter() -> impl IntoView {
+    let (count, set_count) = signal(0);
+    let increment = move |_| set_count.set(count.get()+1);
+
+    view! { 
+        <button on:click=increment>{count}</button>
+    }
 }
